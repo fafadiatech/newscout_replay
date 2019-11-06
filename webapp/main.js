98fb24a79207330ed6ab222ef633a911af8348ff
hardik <hardik@fafadiatech.com>
2019-11-06 16:33:45 +0530

    added electron js

---
+++ webapp/main.js
@@ -0,0 +1,30 @@
+const {app, BrowserWindow} = require('electron');
+
+let win = null;
+
+function createWindow() {
+	win = new BrowserWindow({width:1000, height:800});
+	win.loadURL('http://localhost:3000');
+
+	// open devlopertools
+	win.webContents.openDevTools()
+	win.on('closed', function(){
+		win = null;
+	})
+}
+
+app.on('ready', function(){
+	createWindow();
+})
+
+app.on('activate', () => {
+	if(win === null) {
+		createWindow()
+	}
+})
+
+app.on('window-all-closed', function(){
+	if(process.platform != 'darwin'){
+		app.quit();
+	}
+});
---
+++ webapp/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import HelloWorld from 'newscout-react-components';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <HelloWorld />
+    </div>
+  );
+}
+
+export default App;
