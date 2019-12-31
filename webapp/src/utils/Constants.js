a750ae784996774973615c5ce415500b9ab98c0f
hardik <hardik@fafadiatech.com>
2019-12-31 19:22:16 +0530

    added utils folder

---
+++ webapp/src/utils/Constants.js
@@ -0,0 +1,22 @@
+// export const BASE_URL = window.location.origin;
+export const BASE_URL = "http://139.59.2.130";
+
+export const AD_API_URL = BASE_URL + "/ads/";
+export const ARTICLE_API_URL = BASE_URL + "/api/v1/"
+
+export const CAMPAIGN_URL = AD_API_URL + "campaigns/";
+export const GROUP_URL = AD_API_URL + "adgroups/";
+export const CATEGORIES_CAMPAIGN_URL = AD_API_URL + "categories/";
+export const GROUP_GROUPTYPE_URL = AD_API_URL + "grouptypes/";
+export const ADVERTISEMENT_URL = AD_API_URL + "advertisements/";
+export const ARTICLE_LIST_URL = ARTICLE_API_URL + "articles/";
+export const ARTICLE_SOURCE_LIST_URL = ARTICLE_API_URL + "source/";
+export const ARTICLE_CATEGORY_LIST_URL = ARTICLE_API_URL + "categories/";
+export const ARTICLE_CREATE_URL = ARTICLE_API_URL + "article/create-update/";
+export const ARTICLE_DETAIL_URL = ARTICLE_API_URL + "articles/";
+export const ARTICLE_STATUS_URL = ARTICLE_API_URL + "article/status/";
+export const ARTICLE_DRAFTIMAGE_URL = ARTICLE_API_URL + "article/draft-image/";
+
+export const TRENDING_NEWS = ARTICLE_API_URL + "trending/";
+export const MENUS = ARTICLE_API_URL + "menus/";
+export const ARTICLE_POSTS = ARTICLE_API_URL + "article/search/";
